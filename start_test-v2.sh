98cf6178975d0ed688bd0d03a93690e07354ac81
leokao <yihonggaotw@gmail.com>
2022-03-05 20:30:33 +0800

    change shell interface

--- start_test-v2.sh
+++ start_test-v2.sh
@@ -36,26 +36,70 @@ usage()
   exit 1
 }
 
-job_name=$1; shift
-
-### Parsing the arguments ###
-while getopts 'i:mj:hcrn:' option;
-    do
-      case $option in
-        n	)	namespace=${OPTARG}   ;;
-        c   )   csv=1 ;;
-        m   )   module=1 ;;
-        r   )   enable_report=1 ;;
-        j   )   jmx=${OPTARG} ;;
-        i   )   nb_injectors=${OPTARG} ;;
-        h   )   usage ;;
-        ?   )   usage ;;
-      esac
-done
-
 if [ "$#" -eq 0 ]
   then
     usage
+fi
+
+job_name=$1; shift
+
+### Parsing the arguments ###
+#while getopts 'i:mj:hcrn:' option;
+#while getopts 'namespace:c:m:r:jmx:slave-number:h' option;
+for argument in "$@"
+do
+    if [[ $argument != -* ]]; then
+        continue
+    fi
+    case $argument in
+        -n|--namespace	)	 
+            shift 
+            namespace="$1"
+            ;;
+        -c   )   
+            csv=1 
+            ;;
+        -m   )   
+            module=1 
+            ;;
+        -r   )   
+            enable_report=1 
+            ;;
+        -f|--jmx-filename   ) 
+            shift  
+            jmx_filename="$1" 
+            ;;
+        -a|--artifact-path   )   
+            shift 
+            artifact_path="$1" 
+            ;;
+        --slave-number   )   
+            shift 
+            nb_injectors="$1"
+            ;;
+        -h|--help   )   
+            usage ;; 
+        ?   )   
+            usage ;;
+    esac
+    shift
+done
+
+logit "INFO" "************ Parameter *******************"
+logit "INFO" "namespace: ${namespace}"
+logit "INFO" "artifact-path: ${artifact_path}"
+logit "INFO" "slave-number: ${nb_injectors}"
+logit "INFO" "*******************************"
+
+# Set default value
+if [ -z "${nb_injectors}" ]; then
+    logit "INFO" "set slave number is \"1\""
+    nb_injectors=1
+fi
+
+if [ -z "${jmx_filename}" ]; then
+    logit "INFO" "set jmx_filename is \"main.jmx\""
+    jmx_filename="main.jmx"
 fi
 
 ### CHECKING VARS ###
@@ -70,16 +114,36 @@ if [ -z "${namespace}" ]; then
     namespace=$(awk '{print $NF}' "${PWD}/namespace_export")
 fi
 
-if [ -z "${jmx}" ]; then
+
+if [ -z "${artifact_path}" ]; then
     #read -rp 'Enter the name of the jmx file ' jmx
-    logit "ERROR" "jmx jmeter project not provided!"
+    logit "ERROR" "artifactPath parameter not provided!"
+    usage
+fi
+
+if [ ! -d "${artifact_path}" ]; then
+    #read -rp 'Enter the name of the jmx file ' jmx
+    logit "ERROR" "artifactPath(${artifact_path}) not exists"
+    usage
+fi
+
+jmx_filepath="${artifact_path}/${jmx_filename}"
+
+if [ ! -f "${jmx_filepath}" ]; then
+    #read -rp 'Enter the name of the jmx file ' jmx
+    logit "ERROR" "jmx (${jmx_filepath}) not exists"
     usage
 fi
 
 jmx_dir="${jmx%%.*}"
 
-if [ ! -f "scenario/${jmx_dir}/${jmx}" ]; then
-    logit "ERROR" "Test script file was not found in scenario/${jmx_dir}/${jmx}"
+logit "INFO" "************ JMX Info *******************"
+logit "INFO" "jmx_filename: ${jmx_filename}"
+logit "INFO" "artifact_path: ${artifact_path}"
+logit "INFO" "*******************************"
+
+if [ ! -f "${jmx_filepath}" ]; then
+    logit "ERROR" "Test script file was not found in ${jmx_filepath}"
     usage
 fi
 
@@ -109,23 +173,19 @@ logit "INFO" "Waiting for all slaves pods to be terminated before recreating the
 while [[ $(kubectl -n ${namespace} get pods -l app=${job_slaves_name} -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "" ]]; do echo "$(kubectl -n ${namespace} get pods -l app=${job_slaves_name} )" && sleep 1; done
 
 # Starting jmeter slave pod 
-if [ -z "${nb_injectors}" ]; then
-    logit "WARNING" "Keeping number of injector to 1"
-    kubectl -n "${namespace}" patch job ${job_slaves_name} -p '{"spec":{"parallelism":1}}'
-else
-    logit "INFO" "Scaling the number of pods to ${nb_injectors}. "
-    kubectl -n "${namespace}" patch job ${job_slaves_name} -p '{"spec":{"parallelism":'${nb_injectors}'}}'
-    logit "INFO" "Waiting for pods to be ready"
 
-    end=${nb_injectors}
-    for ((i=1; i<=end; i++))
-    do
-        validation_string=${validation_string}"True"
-    done
+logit "INFO" "Scaling the number of pods to ${nb_injectors}. "
+kubectl -n "${namespace}" patch job ${job_slaves_name} -p '{"spec":{"parallelism":'${nb_injectors}'}}'
+logit "INFO" "Waiting for pods to be ready"
 
-    while [[ $(kubectl -n ${namespace} get pods -l app=${job_slaves_name} -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sed 's/ //g') != "${validation_string}" ]]; do echo "$(kubectl -n ${namespace} get pods -l app=${job_slaves_name} )" && sleep 1; done
-    logit "INFO" "Finish scaling the number of pods."
-fi
+end=${nb_injectors}
+for ((i=1; i<=end; i++))
+do
+    validation_string=${validation_string}"True"
+done
+
+while [[ $(kubectl -n ${namespace} get pods -l app=${job_slaves_name} -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sed 's/ //g') != "${validation_string}" ]]; do echo "$(kubectl -n ${namespace} get pods -l app=${job_slaves_name} )" && sleep 1; done
+logit "INFO" "Finish scaling the number of pods."
 
 #Get Master pod details
 logit "INFO" "Waiting for master pod to be available"
@@ -165,18 +225,18 @@ if [ -n "${module}" ]; then
     logit "INFO" "Finish copying modules in slave pod"
 fi
 
-logit "INFO" "Copying ${jmx} to slaves pods"
+logit "INFO" "Copying ${jmx_filename} to slaves pods"
 logit "INFO" "Number of slaves is ${slave_num}"
 
 for ((i=0; i<end; i++))
 do
-    logit "INFO" "Copying scenario/${jmx_dir}/${jmx} to ${slave_pods[$i]}"
-    kubectl cp -c jmslave "scenario/${jmx_dir}/${jmx}" -n "${namespace}" "${slave_pods[$i]}:/opt/jmeter/apache-jmeter/bin/" &
+    logit "INFO" "Copying ${jmx_filepath} to ${slave_pods[$i]}"
+    kubectl cp -c jmslave "${jmx_filepath}" -n "${namespace}" "${slave_pods[$i]}:/opt/jmeter/apache-jmeter/bin/" &
 done # for i in "${slave_pods[@]}"
 logit "INFO" "Finish copying scenario in slaves pod"
 
-logit "INFO" "Copying scenario/${jmx_dir}/${jmx} into ${master_pod}"
-kubectl cp -c jmmaster "scenario/${jmx_dir}/${jmx}" -n "${namespace}" "${master_pod}:/opt/jmeter/apache-jmeter/bin/" &
+logit "INFO" "Copying ${jmx_filepath} into ${master_pod}"
+kubectl cp -c jmmaster "${jmx_filepath}" -n "${namespace}" "${master_pod}:/opt/jmeter/apache-jmeter/bin/" &
 
 
 logit "INFO" "Installing needed plugins on slave pods"
@@ -184,7 +244,7 @@ logit "INFO" "Installing needed plugins on slave pods"
 
 {
     echo "cd ${jmeter_directory}"
-    echo "sh PluginsManagerCMD.sh install-for-jmx ${jmx} > plugins-install.out 2> plugins-install.err"
+    echo "sh PluginsManagerCMD.sh install-for-jmx ${jmx_filename} > plugins-install.out 2> plugins-install.err"
     echo "jmeter-server -Dserver.rmi.localport=50000 -Dserver_port=1099 -Jserver.rmi.ssl.disable=true >> jmeter-injector.out 2>> jmeter-injector.err &"
     echo "trap 'kill -10 1' EXIT INT TERM"
     echo "java -jar /opt/jmeter/apache-jmeter/lib/jolokia-java-agent.jar start JMeter >> jmeter-injector.out 2>> jmeter-injector.err"
@@ -252,7 +312,7 @@ param_user="-Gthreads=${threads} -Gduration=${duration} -Grampup=${rampup}"
 
 
 if [ -n "${enable_report}" ]; then
-    report_command_line="--reportatendofloadtests --reportoutputfolder /report/report-${jmx}-$(date +"%F_%H%M%S")"
+    report_command_line="--reportatendofloadtests --reportoutputfolder /report/report-${jmx_filename}-$(date +"%F_%H%M%S")"
 fi
 
 echo "slave_array=(${slave_array[@]}); index=${slave_num} && while [ \${index} -gt 0 ]; do for slave in \${slave_array[@]}; do if echo 'test open port' 2>/dev/null > /dev/tcp/\${slave}/1099; then echo \${slave}' ready' && slave_array=(\${slave_array[@]/\${slave}/}); index=\$((index-1)); else echo \${slave}' not ready'; fi; done; echo 'Waiting for slave readiness'; sleep 2; done" > "scenario/${jmx_dir}/load_test.sh"
@@ -260,8 +320,8 @@ echo "slave_array=(${slave_array[@]}); index=${slave_num} && while [ \${index} -
 { 
     echo "echo \"Installing needed plugins for master\""
     echo "cd /opt/jmeter/apache-jmeter/bin" 
-    echo "sh PluginsManagerCMD.sh install-for-jmx ${jmx}" 
-    echo "jmeter ${param_host} ${param_user} ${report_command_line} --logfile /report/${jmx}_$(date +"%F_%H%M%S").jtl --nongui --testfile ${jmx} -Dserver.rmi.ssl.disable=true --remoteexit --remotestart ${slave_list} >> jmeter-master.out 2>> jmeter-master.err &"
+    echo "sh PluginsManagerCMD.sh install-for-jmx ${jmx_filename}" 
+    echo "jmeter ${param_host} ${param_user} ${report_command_line} --logfile /report/${jmx_filename}_$(date +"%F_%H%M%S").jtl --nongui --testfile ${jmx_filename} -Dserver.rmi.ssl.disable=true --remoteexit --remotestart ${slave_list} >> jmeter-master.out 2>> jmeter-master.err &"
     echo "trap 'kill -10 1' EXIT INT TERM"
     echo "java -jar /opt/jmeter/apache-jmeter/lib/jolokia-java-agent.jar start JMeter >> jmeter-master.out 2>> jmeter-master.err"
     echo "wait"
